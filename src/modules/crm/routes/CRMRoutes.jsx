a61a3b838a9c518810dd21f5d1eb7934e8ffb7e5
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-05-27 11:19:21 +0200

    Update CRMRoutes.jsx

--- src/modules/crm/routes/CRMRoutes.jsx
+++ src/modules/crm/routes/CRMRoutes.jsx
@@ -3,6 +3,7 @@ import ProtectedRoute from '../../../components/ProtectedRoute'
 import RoleBasedRoute from '../../../components/RoleBasedRoute'
 import CRMDashboard from '../pages/CRMDashboard'
 import ClientList from '../pages/ClientList'
+import ClientDetail from '../pages/ClientDetail'
 import { USER_ROLES } from '../../../types/authTypes'
 
 export default function CRMRoutes() {
@@ -24,6 +25,16 @@ export default function CRMRoutes() {
           <ProtectedRoute>
             <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]}>
               <ClientList />
+            </RoleBasedRoute>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/clients/:id"
+        element={
+          <ProtectedRoute>
+            <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]}>
+              <ClientDetail />
             </RoleBasedRoute>
           </ProtectedRoute>
         }
