ccef7827be24b288a4f04130664e3c81458adea2
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-05-27 17:23:03 +0200

    Update OperationsRoutes.jsx

--- src/modules/operations/routes/OperationsRoutes.jsx
+++ src/modules/operations/routes/OperationsRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import ProtectedRoute from '../../../components/ProtectedRoute'
 import RoleBasedRoute from '../../../components/RoleBasedRoute'
 import OperationsDashboard from '../pages/OperationsDashboard'
@@ -8,27 +8,165 @@ import { USER_ROLES } from '../../../types/authTypes'
 export default function OperationsRoutes() {
   return (
     <Routes>
-      <Route path="/" element={
-        <ProtectedRoute>
-          <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SUPERVISOR]}>
-            <OperationsDashboard />
-          </RoleBasedRoute>
-        </ProtectedRoute>
-      } />
-      <Route path="/jobs/new" element={
-        <ProtectedRoute>
-          <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SUPERVISOR]}>
-            <CreateJob />
-          </RoleBasedRoute>
-        </ProtectedRoute>
-      } />
-      <Route path="/jobs/:id" element={
-        <ProtectedRoute>
-          <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SUPERVISOR]}>
-            <CreateJob />
-          </RoleBasedRoute>
-        </ProtectedRoute>
-      } />
+      {/* Operations Main Dashboard */}
+      <Route 
+        path="/" 
+        element={
+          <ProtectedRoute>
+            <RoleBasedRoute requiredRoles={[
+              USER_ROLES.SUPER_ADMIN, 
+              USER_ROLES.OPERATIONS_MANAGER, 
+              USER_ROLES.SUPERVISOR,
+              USER_ROLES.HR_MANAGER
+            ]}>
+              <OperationsDashboard />
+            </RoleBasedRoute>
+          </ProtectedRoute>
+        } 
+      />
+
+      {/* Jobs Management */}
+      <Route 
+        path="/jobs" 
+        element={
+          <ProtectedRoute>
+            <RoleBasedRoute requiredRoles={[
+              USER_ROLES.SUPER_ADMIN, 
+              USER_ROLES.OPERATIONS_MANAGER, 
+              USER_ROLES.SUPERVISOR
+            ]}>
+              <OperationsDashboard />
+            </RoleBasedRoute>
+          </ProtectedRoute>
+        } 
+      />
+
+      <Route 
+        path="/jobs/new" 
+        element={
+          <ProtectedRoute>
+            <RoleBasedRoute requiredRoles={[
+              USER_ROLES.SUPER_ADMIN, 
+              USER_ROLES.OPERATIONS_MANAGER, 
+              USER_ROLES.SUPERVISOR
+            ]}>
+              <CreateJob />
+            </RoleBasedRoute>
+          </ProtectedRoute>
+        } 
+      />
+
+      <Route 
+        path="/jobs/:id" 
+        element={
+          <ProtectedRoute>
+            <RoleBasedRoute requiredRoles={[
+              USER_ROLES.SUPER_ADMIN, 
+              USER_ROLES.OPERATIONS_MANAGER, 
+              USER_ROLES.SUPERVISOR,
+              USER_ROLES.CLEANER
+            ]}>
+              <OperationsDashboard />
+            </RoleBasedRoute>
+          </ProtectedRoute>
+        } 
+      />
+
+      {/* Calendar & Scheduling */}
+      <Route 
+        path="/calendar" 
+        element={
+          <ProtectedRoute>
+            <RoleBasedRoute requiredRoles={[
+              USER_ROLES.SUPER_ADMIN, 
+              USER_ROLES.OPERATIONS_MANAGER, 
+              USER_ROLES.SUPERVISOR
+            ]}>
+              <OperationsDashboard />
+            </RoleBasedRoute>
+          </ProtectedRoute>
+        } 
+      />
+
+      {/* Teams Management */}
+      <Route 
+        path="/teams" 
+        element={
+          <ProtectedRoute>
+            <RoleBasedRoute requiredRoles={[
+              USER_ROLES.SUPER_ADMIN, 
+              USER_ROLES.OPERATIONS_MANAGER, 
+              USER_ROLES.SUPERVISOR
+            ]}>
+              <OperationsDashboard />
+            </RoleBasedRoute>
+          </ProtectedRoute>
+        } 
+      />
+
+      {/* Route Planning */}
+      <Route 
+        path="/routes" 
+        element={
+          <ProtectedRoute>
+            <RoleBasedRoute requiredRoles={[
+              USER_ROLES.SUPER_ADMIN, 
+              USER_ROLES.OPERATIONS_MANAGER, 
+              USER_ROLES.SUPERVISOR
+            ]}>
+              <OperationsDashboard />
+            </RoleBasedRoute>
+          </ProtectedRoute>
+        } 
+      />
+
+      <Route 
+        path="/routes/new" 
+        element={
+          <ProtectedRoute>
+            <RoleBasedRoute requiredRoles={[
+              USER_ROLES.SUPER_ADMIN, 
+              USER_ROLES.OPERATIONS_MANAGER
+            ]}>
+              <OperationsDashboard />
+            </RoleBasedRoute>
+          </ProtectedRoute>
+        } 
+      />
+
+      {/* Quality Inspections */}
+      <Route 
+        path="/quality" 
+        element={
+          <ProtectedRoute>
+            <RoleBasedRoute requiredRoles={[
+              USER_ROLES.SUPER_ADMIN, 
+              USER_ROLES.OPERATIONS_MANAGER, 
+              USER_ROLES.SUPERVISOR
+            ]}>
+              <OperationsDashboard />
+            </RoleBasedRoute>
+          </ProtectedRoute>
+        } 
+      />
+
+      {/* Reports */}
+      <Route 
+        path="/reports" 
+        element={
+          <ProtectedRoute>
+            <RoleBasedRoute requiredRoles={[
+              USER_ROLES.SUPER_ADMIN, 
+              USER_ROLES.OPERATIONS_MANAGER
+            ]}>
+              <OperationsDashboard />
+            </RoleBasedRoute>
+          </ProtectedRoute>
+        } 
+      />
+
+      {/* Catch-all redirect */}
+      <Route path="*" element={<Navigate to="/operations" replace />} />
     </Routes>
   )
 }
