1d97ece646502f48857683f6858cac16bbf20266
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-05-27 11:09:28 +0200

    Create CRMRoutes.jsx

---
+++ src/modules/crm/routes/CRMRoutes.jsx
@@ -0,0 +1,33 @@
+import { Routes, Route } from 'react-router-dom'
+import ProtectedRoute from '../../../components/ProtectedRoute'
+import RoleBasedRoute from '../../../components/RoleBasedRoute'
+import CRMDashboard from '../pages/CRMDashboard'
+import ClientList from '../pages/ClientList'
+import { USER_ROLES } from '../../../types/authTypes'
+
+export default function CRMRoutes() {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]}>
+              <CRMDashboard />
+            </RoleBasedRoute>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/clients"
+        element={
+          <ProtectedRoute>
+            <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]}>
+              <ClientList />
+            </RoleBasedRoute>
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  )
+}
