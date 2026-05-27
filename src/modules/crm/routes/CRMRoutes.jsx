752a675bf6393d3c266e734b5876f8885ea60de5
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-05-27 11:28:51 +0200

    Update CRMRoutes.jsx

--- src/modules/crm/routes/CRMRoutes.jsx
+++ src/modules/crm/routes/CRMRoutes.jsx
@@ -9,36 +9,27 @@ import { USER_ROLES } from '../../../types/authTypes'
 export default function CRMRoutes() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]}>
-              <CRMDashboard />
-            </RoleBasedRoute>
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/clients"
-        element={
-          <ProtectedRoute>
-            <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]}>
-              <ClientList />
-            </RoleBasedRoute>
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/clients/:id"
-        element={
-          <ProtectedRoute>
-            <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]}>
-              <ClientDetail />
-            </RoleBasedRoute>
-          </ProtectedRoute>
-        }
-      />
+      <Route path="/" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]}>
+            <CRMDashboard />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+      <Route path="/clients" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]}>
+            <ClientList />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+      <Route path="/clients/:id" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]}>
+            <ClientDetail />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
     </Routes>
   )
 }
