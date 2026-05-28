780b43214f961c2f3c54e727825e61e1d7cbe6bf
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-05-28 12:27:44 +0200

    Update InventoryRoutes.jsx

--- src/modules/inventory/routes/InventoryRoutes.jsx
+++ src/modules/inventory/routes/InventoryRoutes.jsx
@@ -3,6 +3,8 @@ import ProtectedRoute from '../../../components/ProtectedRoute'
 import RoleBasedRoute from '../../../components/RoleBasedRoute'
 import InventoryDashboard from '../pages/InventoryDashboard'
 import StockList from '../pages/StockList'
+import StockIn from '../pages/StockIn'
+import StockOut from '../pages/StockOut'
 import { USER_ROLES } from '../../../types/authTypes'
 
 export default function InventoryRoutes() {
@@ -26,6 +28,27 @@ export default function InventoryRoutes() {
         <ProtectedRoute>
           <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SUPERVISOR]}>
             <StockList />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+      <Route path="/items/new" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SUPERVISOR]}>
+            <StockList />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+      <Route path="/stock-in" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SUPERVISOR]}>
+            <StockIn />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+      <Route path="/stock-out" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SUPERVISOR]}>
+            <StockOut />
           </RoleBasedRoute>
         </ProtectedRoute>
       } />
