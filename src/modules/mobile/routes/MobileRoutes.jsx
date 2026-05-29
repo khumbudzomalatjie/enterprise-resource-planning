0f2d322f55aee9799e3a96b5d692e5e73b79b808
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-05-29 13:28:32 +0200

    Update MobileRoutes.jsx

--- src/modules/mobile/routes/MobileRoutes.jsx
+++ src/modules/mobile/routes/MobileRoutes.jsx
@@ -6,10 +6,14 @@ import ClockInOut from '../pages/ClockInOut'
 import PhotoUpload from '../pages/PhotoUpload'
 import SuppliesRequest from '../pages/SuppliesRequest'
 import MyProfile from '../pages/MyProfile'
+import FieldDashboard from '../pages/FieldDashboard'
+import ActiveCleaners from '../pages/ActiveCleaners'
+import JobPhotos from '../pages/JobPhotos'
 
 export default function MobileRoutes() {
   return (
     <Routes>
+      {/* Cleaner Mobile Routes */}
       <Route path="/" element={<ProtectedRoute><MobileHome /></ProtectedRoute>} />
       <Route path="/jobs" element={<ProtectedRoute><MyJobs /></ProtectedRoute>} />
       <Route path="/jobs/:id" element={<ProtectedRoute><MyJobs /></ProtectedRoute>} />
@@ -18,6 +22,13 @@ export default function MobileRoutes() {
       <Route path="/supplies" element={<ProtectedRoute><SuppliesRequest /></ProtectedRoute>} />
       <Route path="/incident" element={<ProtectedRoute><SuppliesRequest /></ProtectedRoute>} />
       <Route path="/profile" element={<ProtectedRoute><MyProfile /></ProtectedRoute>} />
+
+      {/* Manager Field Operations Routes */}
+      <Route path="/field" element={<ProtectedRoute><FieldDashboard /></ProtectedRoute>} />
+      <Route path="/field/cleaners" element={<ProtectedRoute><ActiveCleaners /></ProtectedRoute>} />
+      <Route path="/field/photos" element={<ProtectedRoute><JobPhotos /></ProtectedRoute>} />
+      <Route path="/field/incidents" element={<ProtectedRoute><FieldDashboard /></ProtectedRoute>} />
+      <Route path="/field/supplies" element={<ProtectedRoute><FieldDashboard /></ProtectedRoute>} />
     </Routes>
   )
 }
